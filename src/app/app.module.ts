854724564b3105f0fa299d6cdf18bff4954621d4
Ruben <ruben.infantes@gmail.com>
2021-04-26 21:33:35 +0200

    actualización
    Actualizado con la navbar a medio hacer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { Navbar2Component } from './navbar2/navbar2.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [				
+    AppComponent,
+      HomeComponent,
+      NavbarComponent,
+      Navbar2Component,
+      FooterComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
