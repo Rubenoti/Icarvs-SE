fa567ff734b4406307efc5f81fd8e72580cea72c
Ruben <ruben.infantes@gmail.com>
2021-04-28 20:23:23 +0200

    Boostrap para Angular
    Noelia la chupa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -8,6 +7,8 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { Navbar2Component } from './navbar2/navbar2.component';
 import { FooterComponent } from './footer/footer.component';
 import { IvyCarouselModule } from 'angular-responsive-carousel';
+import { MdbModule } from 'mdb-angular-ui-kit';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,13 @@ import { IvyCarouselModule } from 'angular-responsive-carousel';
     Navbar2Component,
     FooterComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, IvyCarouselModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    IvyCarouselModule,
+    MdbModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
