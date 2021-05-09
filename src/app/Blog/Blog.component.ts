ca34c8067722b246b01b1e63e3592d860be8be3b
Ruben <ruben.infantes@gmail.com>
2021-05-09 20:26:59 +0200

    Añadido blog

---
+++ src/app/Blog/Blog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Blog',
+  templateUrl: './Blog.component.html',
+  styleUrls: ['./Blog.component.scss']
+})
+export class BlogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
