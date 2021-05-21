a952cc436e904eeb967c55a236ed2dedad1c0ab8
Ruben <ruben.infantes@gmail.com>
2021-05-21 11:47:25 +0200

    Sobre nosotros

--- src/app/registro-usuarios/registro-usuarios.component.ts
+++ src/app/registro-usuarios/registro-usuarios.component.ts
@@ -1,4 +1,4 @@
-import { NoPreloading, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { confirmedValidator } from '../registro-usuarios/validators';
