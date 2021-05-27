583d0cf1e0373db3bc719096d5272e9dae093640
NoeliaHdez <81036982+NoeliaHdez@users.noreply.github.com>
2021-05-27 20:15:33 +0200

    intento de pop up chapucero añadido en home2

--- src/app/Home2/Home2.component.ts
+++ src/app/Home2/Home2.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-Home2',
   templateUrl: './Home2.component.html',
-  styleUrls: ['./Home2.component.scss']
+  styleUrls: ['./Home2.component.scss'],
 })
 export class Home2Component implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
